6110b130f194e99cd74660ffb7dc6717effc7866
Qvvan <90276875+Qvvan@users.noreply.github.com>
2024-09-12 16:31:16 +0300

    Небольшое изменение

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
-from config_data.config import load_config
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from config_data.config import load_config, Config
 from database.init_db.init_db import InitDB
+from handlers import other_handlers, user_handlers
+from keyboards.set_menu import set_main_menu
 
-from logger.logging_config import setup_logging
+logger = logging.getLogger(__name__)
 
 
-def main():
-    setup_logging()
+async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+               '[%(asctime)s] - %(name)s - %(message)s')
 
-    config = load_config()
+    # Выводим в консоль информацию о начале запуска бота
+    logger.info('Starting bot')
+
+    # Загружаем конфиг в переменную config
+    config: Config = load_config()
     db = InitDB(config)
+
+    bot = Bot(
+        token=config.tg_bot.token,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+    dp = Dispatcher()
+
+    await set_main_menu(bot)
+
+    # Регистрируем роутеры в диспетчере
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
