6ffd3315e7c86cadd5f56199b134361f61cd5e55
Ivan Lebedev <90276875+Qvvan@users.noreply.github.com>
2024-09-16 00:39:15 +0300

    Обновление методов и моделей
    Обновил методы и модели. А так же добавил миграции базы данных

--- models/models.py
+++ models/models.py
@@ -1,19 +1,16 @@
 from datetime import datetime
-
-from sqlalchemy import Column, String, Integer, DateTime, Boolean, ForeignKey
+from sqlalchemy import Column, String, Integer, DateTime, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-
 
 class Users(Base):
     __tablename__ = 'users'
 
     user_id = Column(Integer, primary_key=True, autoincrement=True)
-    tg_id = Column(String, unique=True, index=True, nullable=False)
+    tg_id = Column(Integer, unique=True, index=True, nullable=False)
     username = Column(String, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
-
 
 class Services(Base):
     __tablename__ = 'services'
@@ -23,49 +20,45 @@ class Services(Base):
     duration_days = Column(Integer, nullable=False)
     price = Column(Integer, nullable=False)
 
-
 class VPNKeys(Base):
     __tablename__ = 'vpn_keys'
 
     vpn_key_id = Column(Integer, primary_key=True, autoincrement=True)
     key = Column(String, unique=True, nullable=False)
     issued_at = Column(DateTime, default=datetime.utcnow, nullable=False)
-    is_active = Column(Boolean, nullable=False, default=True)
-    is_blocked = Column(Boolean, default=False)
+    is_active = Column(Integer, nullable=False, default=True)
+    is_blocked = Column(Integer, default=False)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
-
 
 class Subscriptions(Base):
     __tablename__ = 'subscriptions'
 
     subscription_id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('users.user_id'), nullable=False)
-    service_id = Column(Integer, ForeignKey('services.service_id'), nullable=False)
-    vpn_key_id = Column(Integer, ForeignKey('vpn_keys.vpn_key_id'), nullable=False)
+    user_id = Column(Integer, nullable=False)  # Removed ForeignKey
+    service_id = Column(Integer, nullable=False)  # Removed ForeignKey
+    vpn_key_id = Column(Integer, nullable=False)  # Removed ForeignKey
     start_date = Column(DateTime, nullable=False)
     end_date = Column(DateTime, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
 
-
 class Logs(Base):
     __tablename__ = 'logs'
 
     log_id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('users.user_id'), nullable=False)
+    user_id = Column(Integer, nullable=False)
     action = Column(String, nullable=False)
-    details = Column(String, nullable=False)  # Convert dictionary to JSON string
+    details = Column(String, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
-
 
 class Transactions(Base):
     __tablename__ = 'transactions'
 
     transaction_id = Column(Integer, primary_key=True, autoincrement=True)
     transaction_code = Column(String, unique=True, nullable=False)
-    service_id = Column(Integer, ForeignKey('services.service_id'), nullable=False)
-    user_id = Column(Integer, ForeignKey('users.user_id'), nullable=False)
+    service_id = Column(Integer, nullable=False)
+    user_id = Column(Integer, nullable=False)
     status = Column(String, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
--- services/services.py
+++ services/services.py
@@ -1,56 +1,61 @@
 import time
 from datetime import datetime, timedelta
+
+from sqlalchemy.exc import SQLAlchemyError
+
 from database.db import DB
 from models.models import Transactions, Subscriptions, VPNKeys
 from lexicon.lexicon_ru import LEXICON_RU
 
 
 async def process_successful_payment(message):
-    await message.answer(text="Спасибо за покупку!")
     await refund_payment(message)
-    transaction = create_transaction(message, 'succesful')
-    transaction_state = DB.get().transactions.add_transaction(transaction)
 
+    connection = DB.get().connection
+    print('Hello')
+    try:
+        print('Я тут')
+        async with connection.begin():
+            print('И тут тоже')
+            await message.answer(text="Спасибо за покупку!")
 
-    vpn_key = DB.get().vpn_keys.get_vpn_key()
-    if vpn_key:
-        if transaction_state:
-            update_vpn_key(str(vpn_key['id']))
-            subscription_created = create_subscription(message, vpn_key['key'])
-            if subscription_created:
-                await send_success_response(message, vpn_key['key'])
+            transaction_state = create_transaction(message, 'successful')
+
+            vpn_key = await DB.get().vpn_keys.get_vpn_key(connection)
+            if vpn_key:
+                if transaction_state:
+                    await update_vpn_key(str(vpn_key['id']), connection)
+                    subscription_created = await create_subscription(message, vpn_key['key'], connection)
+                    if subscription_created:
+                        await send_success_response(message, vpn_key['key'])
+                    else:
+                        await message.answer(text="Не удалось создать подписку. Попробуйте позже.")
+                        raise Exception("Ошибка создания подписки")
+                else:
+                    # Если не удалось сохранить транзакцию
+                    await message.answer(text="Не удалось сохранить транзакцию. Попробуйте позже.")
+                    raise Exception("Ошибка сохранения транзакции")
             else:
-                await message.answer(text="Не удалось создать подписку. Попробуйте позже.")
-                transaction = create_transaction(message, 'отмена: создание подписки')
-                DB.get().transactions.add_transaction(transaction)
-                await refund_payment(message)
-        else:
-            await message.answer(text="Не удалось сохранить транзакцию. Попробуйте позже.")
-            transaction = create_transaction(message, 'отмена транзакции')
-            DB.get().transactions.add_transaction(transaction)
-            await refund_payment(message)
-    else:
-        await message.answer(text="К сожалению, ключи закончились, обратитесь в тех. поддержку")
-        transaction = create_transaction(message, 'отмена: нет ключей')
-        DB.get().transactions.add_transaction(transaction)
-        await refund_payment(message)
+                # Если ключи закончились
+                await message.answer(text="К сожалению, ключи закончились, обратитесь в тех. поддержку")
+                raise Exception("Нет доступных ключей")
 
+    except SQLAlchemyError as e:
+        await refund_payment(message)
+        create_transaction(message, status=str(e))
 
 async def refund_payment(message):
     await message.bot.refund_star_payment(message.from_user.id, message.successful_payment.telegram_payment_charge_id)
 
-
 def create_transaction(message, status: str) -> Transactions:
-    # Разбираем payload
     in_payload = message.successful_payment.invoice_payload.split(':')
 
-    # Получаем необходимые данные
     transaction_code = message.successful_payment.telegram_payment_charge_id
-    service_id = in_payload[0]
-    user_id = str(message.from_user.id)
+    service_id = int(in_payload[0])
+    user_id = message.from_user.id
 
-    # Вызываем функцию добавления транзакции, передавая параметры
-    transaction = DB.get().transactions.add_transaction(
+    # Создание транзакции
+    transaction = DB.get().transactions.create_transaction(
         transaction_code=transaction_code,
         service_id=service_id,
         user_id=user_id,
@@ -59,28 +64,25 @@ def create_transaction(message, status: str) -> Transactions:
 
     return transaction
 
-
-def update_vpn_key(vpn_key_id: str):
-    DB.get().vpn_keys.update_vpn_key(VPNKeys(
+async def update_vpn_key(vpn_key_id: str, connection):
+    await DB.get().vpn_keys.update_vpn_key(VPNKeys(
         id=vpn_key_id,
         is_active=1,
         is_blocked=0,
-    ))
+    ), connection)
 
-
-def create_subscription(message, vpn_key: str) -> bool:
+async def create_subscription(message, vpn_key: str, connection) -> bool:
     in_payload = message.successful_payment.invoice_payload.split(':')
     service_id = in_payload[0]
     durations_days = in_payload[1]
 
-    return DB.get().subscriptions.create_sub(Subscriptions(
+    return await DB.get().subscriptions.create_sub(Subscriptions(
         tg_id=str(message.from_user.id),
         service_id=service_id,
         vpn_key_id=vpn_key,
         start_date=time.time(),
         end_date=(datetime.now() + timedelta(days=int(durations_days))).timestamp(),
-    ))
-
+    ), connection)
 
 async def send_success_response(message, vpn_key: str):
     await message.answer(text=LEXICON_RU['outline_info'])
