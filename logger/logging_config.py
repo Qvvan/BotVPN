e1ea5db0f14fd6ef06c6a5941fbe486b8822a8f5
Ivan Lebedev <90276875+Qvvan@users.noreply.github.com>
2024-10-02 22:40:50 +0300

    fix

--- logger/logging_config.py
+++ logger/logging_config.py
@@ -1,16 +1,8 @@
 import logging
 
+logging.basicConfig(
+    format="%(levelname)s - %(asctime)s - %(name)s - %(filename)s - line: %(lineno)d - %(message)s",
+    level=logging.INFO
+)
+
 logger = logging.getLogger(__name__)
-
-logger.setLevel(logging.INFO)
-
-formatter = logging.Formatter("%(levelname)s - %(asctime)s - %(name)s - %(filename)s - line: %(lineno)d - %(message)s")
-
-file_handler = logging.FileHandler('bot.log')
-file_handler.setFormatter(formatter)
-
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(formatter)
-
-logger.addHandler(file_handler)
-logger.addHandler(console_handler)
